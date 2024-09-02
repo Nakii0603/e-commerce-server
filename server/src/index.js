651abfedb629b9774b402591ecf9b8a1c7b13430
Nakii <guest1@Naakiizs-MacBook-Air.local>
2024-09-03 05:16:47 +0800

    fix codes

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const startServer = async () => {
   const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
   server.applyMiddleware({ app });
-  const PORT = process.env.PORT || 4000;
+  await server.listen({ port: process.env.PORT || 4000 });
   app.listen(PORT, () => {
     console.log(
       `Server is running at http://localhost:${PORT}${server.graphqlPath} 🔥`
